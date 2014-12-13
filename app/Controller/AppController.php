7dba5c7d71b202c3853e7c0543250b0fbfd12a24
Koji Tanaka <tenkoma@gmail.com>
2014-12-13 11:05:06 +0000

    install DebugKit

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -22,6 +22,10 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+    public $components = [
+        'DebugKit.Toolbar',
+    ];
+    
     public $helpers = [
         'Html' => ['className' => 'BoostCake.BoostCakeHtml'],
         'Form' => ['className' => 'BoostCake.BoostCakeForm'],
