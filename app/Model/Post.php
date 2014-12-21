95689f9719c4ca61ad30d9a7027142f35830de32
Koji Tanaka <tenkoma@gmail.com>
2014-12-21 09:25:59 +0000

    DocBlockコメントを追加

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -3,15 +3,25 @@ App::uses('AppModel', 'Model');
 /**
  * Post Model
  *
+ * ブログ記事用モデルです
+ * @copyright php_ci_book
+ * @link https://github.com/tenkoma/blogapp/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @author tenkoma <test@example.com>
  */
 class Post extends AppModel {
+    /**
+     * 一覧表示時のタイトルに使用するカラム名
+     *
+     * @var string
+     */
     public $displayField = 'title';
 
-/**
- * Validation rules
- *
- * @var array
- */
+    /**
+     * バリデーションルール
+     *
+     * @var array
+     */
     public $validate = [
         'title' => [
             'notEmpty' => [
